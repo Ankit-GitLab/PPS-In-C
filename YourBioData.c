6937d3d59940cc19208e9320de93ee37ecc142bd
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-04-27 15:36:25 +0530

    First Commit

---
+++ YourBioData.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // Including standard input-output library
+
+int main() {
+    // Printing bio-data using multiple printf statements
+    printf("----- My Bio-Data -----\n");
+    printf("Name       : Ankit Kumar\n");
+    printf("Father's Name : Dharmendra Sharma\n");
+    printf("Mother's Name : Nilu Devi\n");
+    printf("Date of Birth : 21th May 2007\n");
+    printf("Address    : Bihar, India\n");
+    printf("Mobile No  : +91-6207000824\n");
+    printf("Email      : ankitrocky480@gmail.com\n");
+    printf("Education  : Pursuing B.Tech (Computer Science)\n");
+    printf("Hobbies    : Coding, Reading, Playing Cricket\n");
+
+    return 0; // Return 0 to indicate successful execution
+}
