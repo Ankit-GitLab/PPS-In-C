93c8b988e18cfb28975bdc8325908f6b90a69824
Ankit-GitLab <ankit_246003@saitm.ac.in>
2025-05-03 11:53:54 +0530

    Source Code

--- TextFlieAndDisply.c
+++ TextFlieAndDisply.c
@@ -1,26 +1,38 @@
-#include <stdio.h>  
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main() {
-    FILE *file;  
-    char filename[100];  
-    char ch;  
+    FILE *file;
+    char filename[100];
+    char ch;
+    char cwd[1024];
 
-    
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current directory: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+        exit(1);
+    }
+
     printf("Enter the filename to read: ");
-    scanf("%s", filename);  
+    scanf("%s", filename);
 
-    file = fopen(filename, "r");  
+    file = fopen(filename, "r");
+
     if (file == NULL) {
-        printf("Error opening the file.\n");
-        return 1;  
+        printf("Error: Could not open file '%s'.\n", filename);
+        printf("Make sure the file exists in the above directory and is readable.\n");
+        return 1;
     }
 
     printf("\nContents of the file:\n");
     while ((ch = fgetc(file)) != EOF) {
-        putchar(ch);  
+        putchar(ch);
     }
 
     fclose(file);
 
-    return 0;  
+    return 0;
 }
